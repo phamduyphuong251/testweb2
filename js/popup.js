8652ccda22dd7effdf1f94b7874779889ab3bf48
Duy Phuong Pham <duyphuongpham@Duys-MacBook-Air.local>
2024-04-23 17:16:55 +0700

    update popup

---
+++ js/popup.js
@@ -0,0 +1,138 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var popup = document.getElementById("aiChatbotPopup");
+    var btn = document.getElementById("openPopupBtn1");
+    var span = document.getElementsByClassName("close-popup 1")[0];
+  
+    // Khi nút được nhấp, mở popup
+    btn.onclick = function() {
+      popup.style.display = "block";
+    }
+  
+    // Khi dấu x được nhấp, đóng popup
+    span.onclick = function() {
+      popup.style.display = "none";
+    }
+  
+    // Khi người dùng nhấp vào bất cứ đâu ngoài popup, đóng nó lại
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var popup = document.getElementById("aitrainingPopup");
+    var btn = document.getElementById("openPopupBtn2");
+    var span = document.getElementsByClassName("close-popup 2")[0];
+  
+    // Khi nút được nhấp, mở popup
+    btn.onclick = function() {
+      popup.style.display = "block";
+    }
+  
+    // Khi dấu x được nhấp, đóng popup
+    span.onclick = function() {
+      popup.style.display = "none";
+    }
+  
+    // Khi người dùng nhấp vào bất cứ đâu ngoài popup, đóng nó lại
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var popup = document.getElementById("tuVanQTTTPopup");
+    var btn = document.getElementById("openPopupBtn3");
+    var span = document.getElementsByClassName("close-popup 3")[0];
+  
+    // Khi nút được nhấp, mở popup
+    btn.onclick = function() {
+      popup.style.display = "block";
+    }
+  
+    // Khi dấu x được nhấp, đóng popup
+    span.onclick = function() {
+      popup.style.display = "none";
+    }
+  
+    // Khi người dùng nhấp vào bất cứ đâu ngoài popup, đóng nó lại
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var popup = document.getElementById("recommendationsystemPopup");
+    var btn = document.getElementById("openPopupBtn4");
+    var span = document.getElementsByClassName("close-popup 4")[0];
+  
+    // Khi nút được nhấp, mở popup
+    btn.onclick = function() {
+      popup.style.display = "block";
+    }
+  
+    // Khi dấu x được nhấp, đóng popup
+    span.onclick = function() {
+      popup.style.display = "none";
+    }
+  
+    // Khi người dùng nhấp vào bất cứ đâu ngoài popup, đóng nó lại
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var popup = document.getElementById("dataanalysisPopup");
+    var btn = document.getElementById("openPopupBtn5");
+    var span = document.getElementsByClassName("close-popup 5")[0];
+  
+    // Khi nút được nhấp, mở popup
+    btn.onclick = function() {
+      popup.style.display = "block";
+    }
+  
+    // Khi dấu x được nhấp, đóng popup
+    span.onclick = function() {
+      popup.style.display = "none";
+    }
+  
+    // Khi người dùng nhấp vào bất cứ đâu ngoài popup, đóng nó lại
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var popup = document.getElementById("virtualassistantPopup");
+    var btn = document.getElementById("openPopupBtn6");
+    var span = document.getElementsByClassName("close-popup 6")[0];
+  
+    // Khi nút được nhấp, mở popup
+    btn.onclick = function() {
+      popup.style.display = "block";
+    }
+  
+    // Khi dấu x được nhấp, đóng popup
+    span.onclick = function() {
+      popup.style.display = "none";
+    }
+  
+    // Khi người dùng nhấp vào bất cứ đâu ngoài popup, đóng nó lại
+    window.onclick = function(event) {
+      if (event.target == popup) {
+        popup.style.display = "none";
+      }
+    }
+  });
+  
